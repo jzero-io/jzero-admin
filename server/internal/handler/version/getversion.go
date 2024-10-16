ecb0225309c0f759360d8c5522d6d20b419e13d5
jaron <jaron@jaronnie.com>
2024-10-16 14:12:19 +0800

    feat(server): update

---
+++ server/internal/handler/version/getversion.go
@@ -0,0 +1,29 @@
+package version
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+
+	"server/internal/logic/version"
+	"server/internal/svc"
+	types "server/internal/types/version"
+)
+
+func GetVersion(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.GetVersionRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := version.NewGetVersion(r.Context(), svcCtx)
+		resp, err := l.GetVersion(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
