8b6b3214c1f5a2a4863a6512323a9ccc6ff7b4b5
jaron <jaron@jaronnie.com>
2024-11-04 23:44:56 +0800

    feat(server): update sql desc
    feat(server): update sql desc

--- server/desc/sql/casbin_rule.sql
+++ server/desc/sql/casbin_rule.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `casbin_rule` (
-                               `id` bigint NOT NULL AUTO_INCREMENT,
-                               `ptype` varchar(100) DEFAULT NULL,
-                               `v0` varchar(100) DEFAULT NULL,
-                               `v1` varchar(100) DEFAULT NULL,
-                               `v2` varchar(100) DEFAULT NULL,
-                               `v3` varchar(100) DEFAULT NULL,
-                               `v4` varchar(100) DEFAULT NULL,
-                               `v5` varchar(100) DEFAULT NULL,
-                               PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `ptype` varchar(100) DEFAULT NULL,
+  `v0` varchar(100) DEFAULT NULL,
+  `v1` varchar(100) DEFAULT NULL,
+  `v2` varchar(100) DEFAULT NULL,
+  `v3` varchar(100) DEFAULT NULL,
+  `v4` varchar(100) DEFAULT NULL,
+  `v5` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- server/desc/sql/system_role_menu.sql
+++ server/desc/sql/system_role_menu.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `system_role_menu` (
-                                    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-                                    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                    `create_by` bigint DEFAULT NULL,
-                                    `update_by` bigint DEFAULT NULL,
-                                    `role_id` bigint NOT NULL,
-                                    `menu_id` bigint NOT NULL,
-                                    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_by` bigint DEFAULT NULL,
+  `update_by` bigint DEFAULT NULL,
+  `role_id` bigint NOT NULL,
+  `menu_id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
