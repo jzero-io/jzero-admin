ba97eb13aee8acaea729ebfd2f679c6cbf3677fc
jaron <jaron@jaronnie.com>
2024-11-29 14:27:33 +0800

    feat(server): update error middleware

--- server/internal/middleware/error.go
+++ server/internal/middleware/error.go
@@ -2,12 +2,17 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/jzero-io/jzero-contrib/status"
+	"github.com/spf13/cast"
 )
 
 func ErrorMiddleware(err error) (int, any) {
+	fromError := status.FromError(err)
+
 	return http.StatusOK, Body{
 		Data: nil,
-		Code: "500",
-		Msg:  err.Error(),
+		Code: cast.ToString(fromError.Code()),
+		Msg:  fromError.Error(),
 	}
 }
