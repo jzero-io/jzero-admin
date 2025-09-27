749faf3b9fd7d4bfef85fa6ed7c887887267c524
jaron <jaron@jaronnie.com>
2025-09-27 15:40:31 +0800

    feat(admin): update deploy
    feat(admin): update deploy

--- server/internal/custom/custom.go
+++ server/internal/custom/custom.go
@@ -1,9 +1,13 @@
 package custom
 
 import (
+	"context"
+
+	"github.com/jzero-io/jzero/core/stores/migrate"
 	"github.com/zeromicro/go-zero/rest"
 
 	"github.com/jzero-io/jzero-admin/server/internal/errcodes"
+	"github.com/jzero-io/jzero-admin/server/internal/global"
 )
 
 type Custom struct {
@@ -17,10 +21,14 @@ func New(server *rest.Server) *Custom {
 }
 
 // Init Please add custom logic here.
-func (c *Custom) Init() {
+func (c *Custom) Init() error {
 	errcodes.Register()
+	if err := migrate.Migrate(context.Background(), global.ServiceContext.MustGetConfig().Sqlx.SqlConf); err != nil {
+		return err
+	}
 
 	c.AddRoutes(c.Server)
+	return nil
 }
 
 // Start Please add custom logic here.
